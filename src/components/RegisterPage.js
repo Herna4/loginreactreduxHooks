8eef11f80199393a05b523fc3bed09f0ea07d156
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-19 14:40:34 -0400

    updated npm

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,71 +1,49 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+// import "./styles.css";
 
-export default class RegisterPage extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state={
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: "",
-            password_confirmation: ""
-        }
+const Form = () => {
 
-        this.handleInputChange  = this.handleInputChange.bind(this)
-    }
-
-    handleInputChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    render() {
-        const {firstName, lastName, email, password, password_confirmation} = this.state
-            return (
-                <form>
-                  <input
-                    value={firstName}
-                    onChange={this.handleInputChange}
-                    placeholder="First name"
-                    type="text"
-                    name="firstName"
-                    required
-                  />
-                  <input
-                    value={lastName}
-                    onChange={this.handleInputChange}
-                    placeholder="Last name"
-                    type="text"
-                    name="lastName"
-                    required
-                  />
-                  <input
-                    value={email}
-                    onChange={this.handleInputChange}
-                    placeholder="Email address"
-                    type="email"
-                    name="email"
-                    required
-                  />
-                  <input
-                    value={password}
-                    onChange={this.handleInputChange}
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    required
-                  />
-                  <input
-                    value={password_confirmation}
-                    onChange={this.handleInputChange}
-                    placeholder="Password Confirmation"
-                    type="password"
-                    name="password_confirmation"
-                    required
-                  />
-                  <button type="submit">Submit</button>
-                </form>
-        )
-    }
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <form>
+      <input
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+        placeholder="Last name"
+        type="text"
+        name="lastName"
+        required
+      />
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder="Email address"
+        type="email"
+        name="email"
+        required
+      />
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+export default Form;
